e78198fb6eca8d335e453dbfcb2bb47d08577234
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-03-22 23:58:35 +0200

    Add some test data to the database.

---
+++ scripts/add_test_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO infotv_user(name, password_hash, salt) VALUES ('contributor', 'something', 'random');
+INSERT INTO infotv_theme(name) VALUES ('sport');
+INSERT INTO infotv_font(name) VALUES ('Cantarell');
+INSERT INTO infotv_page(title, content) VALUES ('New of today', 'lol');
+
