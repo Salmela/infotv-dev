6b116caad5e5e2a8dcc99820d20ca32d51750dcd
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-03-29 09:44:26 +0300

    Add more testing data to the scripts.

--- scripts/add_test_data.sql
+++ scripts/add_test_data.sql
@@ -1,5 +1,8 @@
 INSERT INTO infotv_user(name, password_hash, salt) VALUES ('contributor', 'something', 'random');
 INSERT INTO infotv_theme(name) VALUES ('sport');
+INSERT INTO infotv_theme(name) VALUES ('quantum physics');
 INSERT INTO infotv_font(name) VALUES ('Cantarell');
-INSERT INTO infotv_page(title, content) VALUES ('New of today', 'lol');
+INSERT INTO infotv_font(name) VALUES ('Bree serif');
+INSERT INTO infotv_page(title, content) VALUES ('News of today', 'lol');
+INSERT INTO infotv_page(title, content) VALUES ('Cool stuff', 'Hello');
 
