cc45a2a6eba0e25256605f6d9a72f8f53029244e
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-03-29 11:11:18 +0300

    Convert the sql files to work on mysql

--- scripts/add_test_data.sql
+++ scripts/add_test_data.sql
@@ -1,8 +1,8 @@
 INSERT INTO infotv_users(name, password_hash, salt) VALUES ('contributor', 'something', 'random');
 INSERT INTO infotv_themes(name) VALUES ('sport');
 INSERT INTO infotv_themes(name) VALUES ('quantum physics');
-INSERT INTO infotv_fonts(name) VALUES ('Cantarell');
-INSERT INTO infotv_fonts(name) VALUES ('Bree serif');
+INSERT INTO infotv_font_families(name) VALUES ('Cantarell');
+INSERT INTO infotv_font_families(name) VALUES ('Bree serif');
 INSERT INTO infotv_pages(title, content) VALUES ('News of today', 'lol');
 INSERT INTO infotv_pages(title, content) VALUES ('Cool stuff', 'Hello');
 
--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,35 +1,41 @@
 CREATE TABLE infotv_pages (
-    id SERIAL PRIMARY KEY,
+    page_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     title varchar(50) NOT NULL,
     content text NOT NULL,
-    modified date
+    modified date NOT NULL
 );
 
 CREATE TABLE infotv_themes (
-    theme_id SERIAL PRIMARY KEY,
+    theme_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     name char(50) NOT NULL
 );
 
 CREATE TABLE infotv_theme_styles (
-    style_id SERIAL PRIMARY KEY,
-    theme_id SERIAL REFERENCES infotv_theme(theme_id),
-    parameter text,
-    value text
+    style_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    -- style_id SERIAL PRIMARY KEY, --
+    -- theme_id SERIAL REFERENCES infotv_theme(theme_id), --
+    theme_id BIGINT UNSIGNED NOT NULL,
+    parameter text NOT NULL,
+    value text NOT NULL,
+    FOREIGN KEY(theme_id) REFERENCES infotv_themes(theme_id)
 );
 
 CREATE TABLE infotv_font_families (
-    font_family_id SERIAL PRIMARY KEY,
+    font_family_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     name char(50) NOT NULL
 );
 
 CREATE TABLE infotv_font_faces (
-    font_id SERIAL PRIMARY KEY,
-    font_family_id SERIAL REFERENCES infotv_font_family(font_family_id),
-    name char(50) NOT NULL
+    font_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    -- font_id SERIAL PRIMARY KEY, --
+    -- font_family_id SERIAL REFERENCES infotv_font_family(font_family_id), --
+    font_family_id BIGINT UNSIGNED NOT NULL,
+    name char(50) NOT NULL,
+    FOREIGN KEY(font_family_id) REFERENCES infotv_font_families(font_family_id)
 );
 
 CREATE TABLE infotv_users (
-    user_id SERIAL PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     name varchar(50) NOT NULL,
     password_hash char(16) NOT NULL,
     salt char(16) NOT NULL
