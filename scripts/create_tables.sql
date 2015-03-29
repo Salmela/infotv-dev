b4fa12641fcd0a1e45be2e0fc6022f414088a95b
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-03-29 10:00:01 +0300

    Make the db table names as plurals.

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,34 +1,34 @@
-CREATE TABLE infotv_page (
+CREATE TABLE infotv_pages (
     id SERIAL PRIMARY KEY,
     title varchar(50) NOT NULL,
     content text NOT NULL,
     modified date
 );
 
-CREATE TABLE infotv_theme (
+CREATE TABLE infotv_themes (
     theme_id SERIAL PRIMARY KEY,
     name char(50) NOT NULL
 );
 
-CREATE TABLE infotv_theme_style (
+CREATE TABLE infotv_theme_styles (
     style_id SERIAL PRIMARY KEY,
     theme_id SERIAL REFERENCES infotv_theme(theme_id),
     parameter text,
     value text
 );
 
-CREATE TABLE infotv_font_family (
+CREATE TABLE infotv_font_families (
     font_family_id SERIAL PRIMARY KEY,
     name char(50) NOT NULL
 );
 
-CREATE TABLE infotv_font_face (
+CREATE TABLE infotv_font_faces (
     font_id SERIAL PRIMARY KEY,
     font_family_id SERIAL REFERENCES infotv_font_family(font_family_id),
     name char(50) NOT NULL
 );
 
-CREATE TABLE infotv_user (
+CREATE TABLE infotv_users (
     user_id SERIAL PRIMARY KEY,
     name varchar(50) NOT NULL,
     password_hash char(16) NOT NULL,
