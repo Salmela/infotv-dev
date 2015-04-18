ab1cd96a17b74d853ca7f56be1840c1a21b1ca37
Aleksi <aleksi.salmela@helsinki.fi>
2015-04-18 12:58:09 +0300

    Move the not found page to another file.

--- check.php
+++ check.php
@@ -1,7 +1,7 @@
 <?php
 /* check that the file is included otherwice show the 404 not found page */
 if(!defined("ADMIN") && !defined("MAIN")) {
-	echo "<h1>page not found</h1>";
+	include "nofound.html";
 	die();
 }
 ?>
