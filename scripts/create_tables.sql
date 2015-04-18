538f703c11e9428fb0d1bda4291943821ac56bfb
Aleksi <aleksi.salmela@helsinki.fi>
2015-04-18 11:07:03 +0300

    Make the changes of the database structure to scripts.

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,13 +1,13 @@
 CREATE TABLE infotv_pages (
     page_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    title varchar(50) NOT NULL,
-    content text NOT NULL,
-    modified date NOT NULL
+    title VARCHAR(50) NOT NULL,
+    content TEXT NOT NULL,
+    modified BIGINT NOT NULL
 );
 
 CREATE TABLE infotv_themes (
     theme_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    name char(50) NOT NULL
+    name CHAR(50) NOT NULL
 );
 
 CREATE TABLE infotv_theme_styles (
@@ -15,14 +15,14 @@ CREATE TABLE infotv_theme_styles (
     -- style_id SERIAL PRIMARY KEY, --
     -- theme_id SERIAL REFERENCES infotv_theme(theme_id), --
     theme_id BIGINT UNSIGNED NOT NULL,
-    parameter text NOT NULL,
-    value text NOT NULL,
+    parameter TEXT NOT NULL,
+    value TEXT NOT NULL,
     FOREIGN KEY(theme_id) REFERENCES infotv_themes(theme_id)
 );
 
 CREATE TABLE infotv_font_families (
     font_family_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    name char(50) NOT NULL
+    name CHAR(50) NOT NULL
 );
 
 CREATE TABLE infotv_font_faces (
@@ -30,14 +30,14 @@ CREATE TABLE infotv_font_faces (
     -- font_id SERIAL PRIMARY KEY, --
     -- font_family_id SERIAL REFERENCES infotv_font_family(font_family_id), --
     font_family_id BIGINT UNSIGNED NOT NULL,
-    name char(50) NOT NULL,
+    name CHAR(50) NOT NULL,
     FOREIGN KEY(font_family_id) REFERENCES infotv_font_families(font_family_id)
 );
 
 CREATE TABLE infotv_users (
     user_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    name varchar(50) NOT NULL,
-    password_hash char(16) NOT NULL,
-    salt char(16) NOT NULL
+    name VARCHAR(50) NOT NULL,
+    password_hash CHAR(16) NOT NULL,
+    salt CHAR(16) NOT NULL
 );
 
