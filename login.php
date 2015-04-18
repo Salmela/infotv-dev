37bf00ed42e06c7da266211c90e87b780b75c67d
Aleksi <aleksi.salmela@helsinki.fi>
2015-04-18 13:35:42 +0300

    Split the login page to view and controller.

--- login.php
+++ login.php
@@ -24,25 +24,6 @@ if(isset($error)) {
 } else {
 	$login = new Login(NULL);
 }
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" />
-	<title>Login</title>
 
-	<link rel="stylesheet" href="/infotv-dev/style.css" />
-</head>
-<body>
-<div id="login-page">
-	<h1>Login</h1>
-<?php $login->errorMsg(); ?>
-<form action="<?php $login->formUrl(); ?>" method="post">
-	<div class="row">Username:<input type="text" name="user" /></div>
-	<div class="row">Password:<input type="password" name="password" /></div>
-	<div class="buttons"><input type="submit" value="Login"></div>
-	<p>If you have forgotten your password, you are out of luck</p>
-</form>
-</div>
-</body>
-</html>
+include "login_page.php";
+?>
