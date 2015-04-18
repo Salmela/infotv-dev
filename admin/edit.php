24fd9b81301aba29c08c6f36447099e58cae14a2
Aleksi <aleksi.salmela@helsinki.fi>
2015-04-18 10:56:28 +0300

    Initial code for hooking up the page editing page to the database.

--- admin/edit.php
+++ admin/edit.php
@@ -1,4 +1,60 @@
-<?php require_once("../check.php"); ?>
+<?php
+require_once("../check.php");
+require_once("../backend/slides.php");
+
+class SlideEdit {
+	var $slides;
+	var $current;
+	var $id;
+	function __construct() {
+		$this->slides = new Slides();
+		$this->current = NULL;
+		$this->id = $_GET["id"];
+
+		if(isset($this->id)) {
+			$this->current = $this->slides->getById($this->id);
+		}
+	}
+
+	function handleActions() {
+		if(isset($_POST["back"])) {
+			header("location: ?page=slides");
+			return false;
+
+		} else if(isset($_POST["delete"])) {
+			if(isset($this->current)) {
+				$slides->delete($this->id);
+			}
+			header("location: ?page=slides");
+			return false;
+
+		} else if(isset($_POST["save"])) {
+			if(isset($this->current)) {
+				$this->slides->update($this->id, $_POST["title"], $_POST["content"]);
+			} else {
+				$this->slides->create($this->id, $_POST["title"], $_POST["content"]);
+			}
+		}
+		return true;
+	}
+
+	function getTitle() {
+		if(isset($this->current)) {
+			echo $this->current->getTitle();
+		}
+	}
+
+	function getContent() {
+		if(isset($this->current)) {
+			echo $this->current->getContent();
+		}
+	}
+}
+$page = new SlideEdit();
+if(!$page->handleActions()) {
+	return;
+}
+?>
 <script type="text/javascript" src="scripts/tiny_mce/tiny_mce.js"></script>
 
 <script type="text/javascript">
@@ -17,14 +73,14 @@ tinyMCE.init({
 	//content_css : "css/content.css"
 
 });
-
 </script>
-<form>
-	<input type="text" name="title" id="title" placeholder="Title" />
-	<textarea name="content" id="content-text"></textarea>
+<form action="#" method="post">
+	<input type="text" name="title" id="title" placeholder="Title" value="<?php $page->getTitle(); ?>" />
+	<textarea name="content" id="content-text"><?php $page->getContent(); ?></textarea>
 	<select name="theme"><option name="default">Default theme</option></select>
 	<div id="buttons">
 		<input type="submit" value="Back" name="back" class="button" />
+		<input type="submit" value="Delete" name="delete" class="button" />
 		<input type="submit" value="Save" name="save" class="button" />
 	</div>
 </form>
