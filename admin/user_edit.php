f78ea8ca0b4a542005cb3bc37d1b815bb1315cbe
Aleksi <aleksi.salmela@helsinki.fi>
2015-04-28 04:43:11 +0300

    Make it possible to save user.

--- admin/user_edit.php
+++ admin/user_edit.php
@@ -1,5 +1,17 @@
-<?php require_once(dirname(__FILE__) . "/../check.php"); ?>
-<input type="text" name="title" id="title" placeholder="Username" />
+<?php
+require_once(dirname(__FILE__) . "/../user_edit_ctrl.php");
+$user = new Users();
+?>
+<input type="text" name="name" id="title" placeholder="Username" value="<?php $user->getName() ?>" />
 <div class="settings-section">
 	<h2>Reset password</h2>
+	<table>
+		<tr><td>Password: </td><td><input type="input" name="password" /></td></tr>
+		<tr><td>Repeat password: </td><td><input type="input" name="repeat" /></td></tr>
+	</table>
+</div>
+<div id="buttons">
+	<input type="submit" value="Back" name="back" class="button" />
+	<input type="submit" value="Delete" name="delete" class="button" />
+	<input type="submit" value="Save" name="save" class="button" />
 </div>
---
+++ admin/user_edit_ctrl.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once(dirname(__FILE__) . "/../check.php");
+require_once(dirname(__FILE__) . "/../backend/users.php");
+
+class UserEdit {
+	var $users;
+	var $current;
+	var $id;
+
+	function __construct() {
+		$this->users = new Users();
+		$this->current = NULL;
+		$this->id = $_GET["id"];
+
+		if(isset($this->id)) {
+			$this->current = $this->users->getById($this->id);
+		}
+	}
+
+	function handleActions() {
+		global $panel;
+
+		if(isset($_POST["back"])) {
+			header("location: ". $_SERVER["SCRIPT_NAME"] ."?page=users");
+			return false;
+
+		} else if(isset($_POST["delete"])) {
+			if(isset($this->current)) {
+				$this->users->remove($this->id);
+			}
+			header("location: ". $_SERVER["SCRIPT_NAME"] ."?page=users");
+			return false;
+
+		} else if(isset($_POST["save"])) {
+			if($_POST["password"] == $_POST["repeat"]) {
+				$panel->appendError("Password were inequal.");
+				return true;
+			}
+			if(isset($this->current)) {
+				$this->users->update($this->id, $_POST["name"], $_POST["password"]);
+			} else {
+				$this->users->create($this->id, $_POST["name"], $_POST["password"]);
+			}
+		}
+		return true;
+	}
+
+	function getName() {
+		if(isset($this->current)) {
+			echo $this->current->getName();
+		}
+	}
+}
+?>
