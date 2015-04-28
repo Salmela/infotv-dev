459f1896c62f075e5c5ac4eae8e128bc4bbe6867
Aleksi <aleksi.salmela@helsinki.fi>
2015-04-28 10:02:30 +0300

    Make the user edit page a form.

--- admin/user_edit.php
+++ admin/user_edit.php
@@ -3,6 +3,7 @@ require_once(dirname(__FILE__) . "/user_edit_ctrl.php");
 $page = new UserEdit();
 if(!$page->handleActions()) return;
 ?>
+<form action="#" method="post">
 <input type="text" name="name" id="title" placeholder="Username" value="<?php $page->getName() ?>" />
 <div class="settings-section">
 	<h2>Reset password</h2>
@@ -16,3 +17,4 @@ if(!$page->handleActions()) return;
 	<input type="submit" value="Delete" name="delete" class="button" />
 	<input type="submit" value="Save" name="save" class="button" />
 </div>
+</form>
