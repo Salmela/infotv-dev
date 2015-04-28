55ed20f93805fb0cc14fefb1acf10edc8f7a1c66
Aleksi <aleksi.salmela@helsinki.fi>
2015-04-28 13:04:33 +0300

    Hook up the themes list to the backend.

--- admin/themes_ctrl.php
+++ admin/themes_ctrl.php
@@ -1,9 +1,10 @@
 <?php
 
 require_once(dirname(__FILE__) . "/../check.php");
+require_once(dirname(__FILE__) . "/../backend/themes.php");
 require_once(dirname(__FILE__) . "/table.php");
 
-function func($row) {
+function func($theme) {
 	return array(
 		"<a href=\"?page=theme-edit&id=134\">Sport</a>",
 		"12.3.2015",
@@ -11,7 +12,9 @@ function func($row) {
 	);
 }
 
-$table = new Table(NULL, array("Name", "Created", "Author"), array());
+$themes = new Themes();
+$table = new Table(NULL, array("Name", "Created", "Rights", "Author"), $themes->getAll());
+#$table = new Table(NULL, array("Name", "Created", "Author"), array());
 $table->setTransformFunc("func");
 
 ?>
