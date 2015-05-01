bb18e9f42ebfd6d137d17924d7d3e59467ca0872
Aleksi <aleksi.salmela@helsinki.fi>
2015-05-01 14:24:08 +0300

    Fix the next and prev links at the index.

--- index.php
+++ index.php
@@ -6,11 +6,29 @@ class IndexPage {
 	var $slides;
 	var $pageID;
 	var $page;
+	var $prevPage, $nextPage;
+
 	function __construct($id) {
 		$this->slides = new Slides();
 		$this->pageID = $id;
 		$slides = new Slides();
-		$this->page = $slides->getById($id);
+
+		$pages = $slides->getAll(false);
+		for($i = 0; $i < count($pages); $i++) {
+			$page = $pages[$i];
+			if($page->getId() == $id) {
+				$this->page = $page;
+				break;
+			}
+		}
+		if($i == count($pages)) {
+			$i = 0;
+			$this->page = $pages[0];
+		}
+		$this->prevPage = $pages[$i == 0 ? count($pages) - 1 : $i - 1];
+		$this->nextPage = $pages[$i == count($pages) - 1 ? 0 : $i + 1];
+
+		$this->page = $slides->getById($this->page->getId());
 	}
 	function title() {
 		echo "Infotv thingy";
@@ -23,17 +41,13 @@ class IndexPage {
 		echo $this->page->getContent();
 	}
 	function nav() {
-		$next = "618";
-		$prev = "43";
-
-		if($prev) {
-			echo "\t\t<div id=\"prev\"><a href=\"index.php?id=". $prev ."\">Prev</a></div>";
+		if($this->prevPage) {
+			echo "\t\t<div id=\"prev\"><a href=\"index.php?id=". $this->prevPage->getId() ."\">Prev</a></div>\n";
 		}
 
-		if($next) {
-			echo "\t\t<div id=\"next\"><a href=\"index.php?id=". $next ."\">Next</a></div>";
+		if($this->nextPage) {
+			echo "\t\t<div id=\"next\"><a href=\"index.php?id=". $this->nextPage->getId() ."\">Next</a></div>\n";
 		}
-		echo "";
 	}
 }
 
