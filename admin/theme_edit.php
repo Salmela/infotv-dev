8db0ac97d0479cd7a22de3cf008ed53ca150d685
Aleksi <aleksi.salmela@helsinki.fi>
2015-05-03 22:51:27 +0300

    Implement theme_edit.php.

--- admin/theme_edit.php
+++ admin/theme_edit.php
@@ -1,2 +1,9 @@
-<?php require_once(dirname(__FILE__) . "/../check.php"); ?>
+<?php
+require_once(dirname(__FILE__) . "/theme_edit_ctrl.php");
+
+$page = new ThemeEdit();
+if(!$page->handleActions()) {
+	return;
+}
+?>
 <input type="text" name="title" id="title" placeholder="Theme name" />
---
+++ admin/theme_edit_ctrl.php
@@ -0,0 +1,48 @@
+<?php
+require_once(dirname(__FILE__) . "/../check.php"); 
+require_once(dirname(__FILE__) . "/../backend/themes.php");
+
+class ThemeEdit {
+	var $themes;
+	var $current;
+	var $id;
+
+	function __construct() {
+		$this->themes = new Themes();
+		$this->current = NULL;
+		$this->id = $_GET["id"];
+
+		if(isset($this->id)) {
+			$this->current = $this->themes->getById($this->id);
+		}
+	}
+
+	function handleActions() {
+		if(isset($_POST["back"])) {
+			header("location: ". $_SERVER["SCRIPT_NAME"] ."?page=themes");
+			return false;
+
+		} else if(isset($_POST["delete"])) {
+			if(isset($this->current)) {
+				$this->slides->remove($this->id);
+			}
+			header("location: ". $_SERVER["SCRIPT_NAME"] ."?page=themes");
+			return false;
+
+		} else if(isset($_POST["save"])) {
+			if(isset($this->current)) {
+				$this->themes->update($this->id, $_POST["title"], $_POST["content"]);
+			} else {
+				$this->themes->create($this->id, $_POST["title"], $_POST["content"]);
+			}
+		}
+		return true;
+	}
+
+	function getTitle() {
+		if(isset($this->current)) {
+			echo $this->current->getName();
+		}
+	}
+}
+?>
