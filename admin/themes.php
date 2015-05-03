16c3fac5bd0fc37d4ca478d5b12e21d0ccc23621
Aleksi <aleksi.salmela@helsinki.fi>
2015-05-03 22:51:01 +0300

    Fix new theme link.

--- admin/themes.php
+++ admin/themes.php
@@ -1,7 +1,7 @@
 <?php require_once(dirname(__FILE__) . "/themes_ctrl.php"); ?>
 
 <h1>Themes</h1>
-<div id="new-button"><a class="button" href="?page=edit&id=new">New theme</a></div>
+<div id="new-button"><a class="button" href="?page=theme-edit&id=new">New theme</a></div>
 <p>List of all the themes in the system.</p>
 
 <?php $table->get(); ?>
