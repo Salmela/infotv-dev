6214e7d35f4b1b7748c4999f4dfdb8398ea4cf1a
Aleksi <aleksi.salmela@helsinki.fi>
2015-05-03 23:59:21 +0300

    Change the font list title.

--- admin/fonts.php
+++ admin/fonts.php
@@ -1,6 +1,6 @@
 <?php require_once(dirname(__FILE__) . "/fonts_ctrl.php"); ?>
 
-<h1>Font families</h1>
+<h1>Font families - Not functional</h1>
 <div id="new-button"><a class="button" href="?page=font-edit&id=new">New font family</a></div>
 <p>List of all the fonts in the system.</p>
 <?php $table->get(); ?>
